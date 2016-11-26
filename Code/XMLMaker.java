86db39ac2254c78661e20236caeb1afe0488ff44
DESKTOP-FC268D6\musth <hydrophilictrend@aol.com>
2016-11-26 18:19:47 -0500

    Update XSD with datatypes

--- Code/XMLMaker.java
+++ Code/XMLMaker.java
@@ -15,29 +15,93 @@ public class XMLMaker {
 //		
 //	}
 	
-	public static void generateXSD(ArrayList<String> input ){
+	public static void generateXSD(ArrayList<String> input)
+    {
 		
 		String printOut = "";
+        String[] tables = input.get(0).replaceAll(" ", "").split(",");
+        String dataType;
 		
 		cleanArray(input);
-		
-		printOut += "<?xml version = \"1.0\"?>\n";
-		printOut += "<xs:schema xmlns:xs = \"http://www.w3.org/2001/XMLSchema\">\n";
-		printOut += "<xs:element name = '" + input.get(0) + "XSDnew\" elementFormDefault=\"qualified\"attributeFormDefault=\"qualified\">\n";
-		printOut += "<xsd:complexType name=\"" + input.get(0) + "\">\n";
-		for(int j = 1; j < input.size(); j++)
-			printOut += "<xsd:element name=\"" + input.get(j) + "\" type=\"xsd:string\" maxOccurs=\"1\" minOccurs=\"1\" />\n";
-		
-		printOut += "</xsd:complexType >\n";
-		printOut += "</schema>\n\n";
-		
-		if (nameFile){
-			createFile(printOut, filename += ".xsd");
-		}
-		System.out.println(printOut);
-		
+
+        printOut += "<?xml version = \"1.0\"?>\n";
+        printOut += "<xs:schema xmlns:xs = \"http://www.w3.org/2001/XMLSchema\">\n";
+        printOut += "<xs:element name = \"" + filename + "\" elementFormDefault=\"qualified\"attributeFormDefault=\"qualified\">\n";
+        for (int i = 0; i < tables.length; i++)
+        {
+            printOut += "<xsd:complexType name=\"" + tables[i] + "\">\n";
+            for(int j = 1; j < input.size(); j++)
+            {
+                dataType = getDataType(tables[i], input.get(j).replaceAll(" ", ""));
+                if (dataType != null)
+                    printOut += "<xsd:element name=\"" + input.get(j) + "\" type=\"xsd:" + dataTypeConv(dataType) + " />\n";
+                dataType = null;
+            }
+            printOut += "</xsd:complexType >\n";
+
+//		if (nameFile){
+//			createFile(printOut, filename += ".xsd");
+//		}
+        }
+        printOut += "</schema>\n\n";
+        System.out.println(printOut);
 	}
 	
+	public static String getDataType(String table, String colName)
+	{
+        try
+        {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load driver.");
+        }
+        try
+        {
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@olympia.unfcsd.unf.edu:1521:dworcl", "team3", "unfpdm");
+            Statement stmt = conn.createStatement();
+            ResultSet res = stmt.executeQuery("Select COLUMN_NAME, data_type from user_tab_columns where TABLE_NAME = '" + table + "'");
+
+            res.next();
+            while(!colName.toLowerCase().matches(res.getString(1).toLowerCase()))
+                res.next();
+
+            String dataType = res.getString(2);
+            stmt.close();
+            return dataType;
+        }
+        catch (SQLException f)
+        {
+            return null;
+        }
+	}
+	
+	public static String dataTypeConv(String dataType)
+	{
+		dataType = dataType.toLowerCase();
+		if (dataType.matches("(character|varchar|character varying)(.*)"))
+			return "string";
+		else if (dataType.matches("boolean"))
+			return "boolean";
+		else if (dataType.matches("(integer|smallint|bigint|decimal|numeric|number)(.*)"))
+			return "decimal";
+		else if (dataType.matches("(float|real)(.*)"))
+			return "float";
+		else if (dataType.matches("double precision"))
+			return "double";
+		else if (dataType.matches("interval"))
+			return "duration";
+		else if (dataType.matches("timestamp"))
+			return "dateTime";
+		else if (dataType.matches("time"))
+			return "time";
+		else if (dataType.matches("date"))
+			return "date";
+		else
+			return null;
+	}
 	
 	public static void DTD(ArrayList<String> tableInfo ){
 		ArrayList<String> dtdLines = new ArrayList<String>();
@@ -124,8 +188,12 @@ public static ArrayList<String> cleanArray(ArrayList<String> input){
 		
 		try {
 			runQuery(tableData, tableInfo); //fetch sql query
+			System.out.println(tableData);
+			System.out.println(tableInfo);
 		} catch (SQLException e) {
-			System.out.println("Issue with connection");
+			System.out.println("Issue with connection. Please check your internet\n");
+			
+			return;
 		}
 		
 		if (tableInfo.get(0).contains(",")){						//check for the main table
