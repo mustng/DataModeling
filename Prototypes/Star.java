1134eae231b1621ee374c898d7b7258a0ec8d0a3
Logan McManus <n00835272@ospreys.unf.edu>
2016-11-28 18:04:15 -0500

    can do multiple tables now

--- Prototypes/Star.java
+++ Prototypes/Star.java
@@ -8,7 +8,7 @@ import oracle.jdbc.driver.OracleDriver;
  */
 public class Star
 {
-    public static ArrayList<String> getAttributes(String table) throws SQLException
+    public static ArrayList<String> getAttributes(ArrayList<String> table) throws SQLException
     {
         ArrayList<String> atts = new ArrayList<>();
         try
@@ -20,9 +20,12 @@ public class Star
         }
         Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@olympia.unfcsd.unf.edu:1521:dworcl", "team3", "unfpdm");
         Statement stmt = conn.createStatement();
-        ResultSet res = stmt.executeQuery("Select COLUMN_NAME, data_type from user_tab_columns where TABLE_NAME = '"+ table +"'");
-        while( res.next() )
-            atts.add(res.getString(1));
+        for (int i = 0; i < table.size(); i++)
+        {
+            ResultSet res = stmt.executeQuery("Select COLUMN_NAME, data_type from user_tab_columns where TABLE_NAME = '"+ table.get(i) +"'");
+            while( res.next() )
+                atts.add(res.getString(1));
+        }
         stmt.close();
 
         return atts;
@@ -30,7 +33,10 @@ public class Star
 
     public static void main(String args[]) throws SQLException
     {
-        ArrayList<String> qe = getAttributes("P");
+        ArrayList<String> in = new ArrayList<String>();
+        in.add("P");
+        in.add("S");
+        ArrayList<String> qe = getAttributes(in);
         for (int i = 0; i < qe.size(); i++)
             System.out.println(qe.get(i));
     }
